5d2a7fc9d569141e388c92768fe44f157fe1545a
danya <danissim00o@yandex.ru>
2023-07-21 22:50:00 +0700

    fix: postman tests

--- src/main/java/ru/practicum/shareit/validation/CheckDateValidator.java
+++ src/main/java/ru/practicum/shareit/validation/CheckDateValidator.java
@@ -7,6 +7,7 @@ import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDateTime;
 
 public class CheckDateValidator implements ConstraintValidator<StartBeforeEndDateValid, BookingSavingDto> {
+
     @Override
     public void initialize(StartBeforeEndDateValid constraintAnnotation) {
     }
