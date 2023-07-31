4593de7b3f8dab4546fc1123c88094753ad19ec0
danya <danissim00o@yandex.ru>
2023-07-31 17:16:23 +0700

    feat: init

--- src/main/java/ru/practicum/shareit/exception/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/exception/NotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NotAvailableException extends RuntimeException {
-    public NotAvailableException(String message) {
-        super(message);
-    }
-}
